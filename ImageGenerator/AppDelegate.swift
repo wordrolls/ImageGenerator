b4e0ca93d9773a4f63aeabde64e23e665d495356
Sudeep <sdpjswl1@gmail.com>
2022-01-28 04:08:07 +0530

    Refactored project and updated settings

---
+++ ImageGenerator/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ImageGenerator
+//
+//  Created by sudeep on 03/12/19.
+//  Copyright © 2019 Word Rolls. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate
+{
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
